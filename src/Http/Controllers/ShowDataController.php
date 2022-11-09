4344cb87f085d7cdeb172243b1f514a6d5d55aa3
Pavlo Poberezhnyi <poberezhnyi_pp@groupbwt.com>
2022-11-09 23:16:22 +0200

    version1.0

---
+++ src/Http/Controllers/ShowDataController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Test\Show\Http\Controllers;
+
+class ShowDataController extends Controller
+{
+    public function index()
+    {
+        dd('version 1.0');
+    }
+}
